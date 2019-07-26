f1495bb9b1c2ee4a0206305b580b8b3d149c106c
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-07-26 11:30:25 +0100

    -wrote test for two endpoints

--- backend/models/users.test.js
+++ backend/models/users.test.js
@@ -1,1 +1,35 @@
 const server = require('../api/server');
+const request = require('supertest')
+const db = require('../database/dbConfig')
+// const User = require('./jokes');
+
+beforeAll(async () => {
+    await db('users').truncate()
+})
+describe('api/register', () => {
+    it('should register new user', () => {
+        request(server)
+            .post('/api/register')
+            .send({
+                username: "Yusuf",
+                password: '123456'
+            })
+            .then(res => {
+                expect(res.status).toEqual(201)
+            })
+    })
+});
+
+describe('api/login', () => {
+    it('sholud return error with invalid password', () => {
+        request(server)
+            .post('/api/login')
+            .send({
+                username: "Yusuf",
+                password: '1234567'
+            })
+            .then(res => {
+                expect(res.status).toEqual(400)
+            })
+    })
+})
